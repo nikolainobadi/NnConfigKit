6ce1f652832ebd35b665eb90f57700576e4abcb9
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-05-05 15:09:32 -0700

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "NnConfigGenKit",
+    products: [
+        .library(
+            name: "NnConfigGenKit",
+            targets: ["NnConfigGenKit"]
+        ),
+        .executable(
+            name: "NnConfigGenKitExecutable",
+            targets: ["NnConfigGenKitExecutable"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.0"),
+        .package(url: "https://github.com/kareman/SwiftShell.git", from: "5.1.0"),
+        .package(url: "https://github.com/JohnSundell/Files.git", from: "4.0.0")
+    ],
+    targets: [
+        .target(
+            name: "NnConfigGenKit",
+            dependencies: [
+                "Files",
+                "SwiftShell",
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]
+        ),
+        .executableTarget(
+            name: "NnConfigGenKitExecutable",
+            dependencies: ["NnConfigGenKit"]
+        )
+    ]
+)
