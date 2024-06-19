2084204d51f0581fc642ad2ddce6acc8c87bb022
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-06-19 14:03:37 -0700

    added more methods to NnConfigManager

--- Sources/NnConfigKit/NnConfigManager.swift
+++ Sources/NnConfigKit/NnConfigManager.swift
@@ -1,6 +1,6 @@
 //
-//  File.swift
-//  
+//  NnConfigManager.swift
+//
 //
 //  Created by Nikolai Nobadi on 6/19/24.
 //
@@ -34,6 +34,56 @@ public extension NnConfigManager {
         let configData = try JSONEncoder.prettyOutput().encode(config)
         
         try configFile.write(configData)
+    }
+    
+    func appendTextToFileIfNeeded(text: String, fileToUpdate: File, asNewLine: Bool) throws {
+        let existingContents = try fileToUpdate.readAsString()
+        
+        if !existingContents.contains(text) {
+            try fileToUpdate.append(asNewLine ? "\n\(text)" : text)
+        }
+    }
+    
+    func removeTextFromFile(text: String, fileToUpdate: File) throws {
+        let existingContents = try fileToUpdate.readAsString()
+        var lines = existingContents.components(separatedBy: .newlines)
+        lines.removeAll { line in
+            line == text.trimmingCharacters(in: .whitespacesAndNewlines)
+        }
+        
+        let updatedContents = lines.joined(separator: "\n")
+        
+        try fileToUpdate.write(updatedContents)
+    }
+}
+
+
+// MARK: - NestedConfigFiles
+public extension NnConfigManager {
+    func saveNestedConfigFile(contents: String, nestedFilePath: String) throws {
+        let configFolder = try createFolderIfNeeded(path: Config.configFolderPath)
+        let nestedFile = try configFolder.createFileIfNeeded(at: nestedFilePath)
+        
+        try nestedFile.write(contents)
+    }
+    
+    func deletedNestedConfigFile(nestedFilePath: String) throws {
+        if let nestedFile = try? Folder(path: Config.configFolderPath).file(at: nestedFilePath) {
+            try nestedFile.delete()
+        }
+    }
+    
+    func appendTextToNestedConfigFileIfNeeded(text: String, nestedFilePath: String, asNewLine: Bool = true) throws {
+        let configFolder = try createFolderIfNeeded(path: Config.configFolderPath)
+        let nestedFile = try configFolder.createFileIfNeeded(at: nestedFilePath)
+        
+        try appendTextToFileIfNeeded(text: text, fileToUpdate: nestedFile, asNewLine: asNewLine)
+    }
+    
+    func removeTextFromNestedConfigFile(text: String, nestedFilePath: String) throws {
+        if let nestedFile = try? Folder(path: Config.configFolderPath).file(at: nestedFilePath) {
+            try removeTextFromFile(text: text, fileToUpdate: nestedFile)
+        }
     }
 }
 
